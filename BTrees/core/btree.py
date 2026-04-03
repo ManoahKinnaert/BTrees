ae6119359c45e3aa30734476587c107ce5f87d70
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-03 14:00:51 +0200

    Started basic BST implementation

--- BTrees/core/btree.py
+++ BTrees/core/btree.py
@@ -1,11 +1,25 @@
 from BTrees.core.node import Node
 
 class BTree:
-    def __init__(self):
-        self._root = None
+    def __init__(self, root: Node=None):
+        self._root: Node = root
 
     def insert(self, node: Node):
-        pass
+        if self._root is None: self._root = node
+        else:
+            current = self._root
+            spot_found = False
+            while not spot_found:
+                if node.get_value() < current.get_value():
+                    if current.get_left() is None:
+                        spot_found = True
+                        self._root.set_left(node)
+                    else: current = current.get_left()
+                else:
+                    if current.get_right() is None:
+                        spot_found = True
+                        self._root.set_right(node)
+                    else: current = current.get_right()
 
     def remove(self, node: Node):
         pass
--- BTrees/core/node.py
+++ BTrees/core/node.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 class Node:
-    def __init__(self, value, left=None, right=None, is_root=False):
-        self._value = value
-        self._left = left
-        self._right = right
-        self._is_root = is_root
+    def __init__(self, value: Any, left: Node=None, right: Node=None, is_root: bool=False):
+        self._value: Any = value
+        self._left: Node = left
+        self._right: Node = right
+        self._is_root: bool = is_root
 
     def is_root(self): return self._is_root
 
@@ -12,3 +14,7 @@ class Node:
     def get_left(self): return self._left
 
     def get_right(self): return self._right
+
+    def set_left(self, node: Node): self._left = node
+
+    def set_right(self, node: Node): self._right = node
